baf49fae355149f64e0c0520b540f067017708e7
jai shankar <67233202+jaishankar510@users.noreply.github.com>
2023-07-15 16:44:14 +0530

    Update settings.py

--- E_Authentication/settings.py
+++ E_Authentication/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "jaishankark510@gmail.com"
-EMAIL_HOST_PASSWORD = 'cwpjbtqakhetkvux'
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ''
